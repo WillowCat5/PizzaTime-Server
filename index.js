ca975b0779b094a3c3295f8e58e42d61dc87ef22
Bryan Scott <bscott@tooeletech.edu>
2019-06-11 17:47:14 -0600

    Getting closer...

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-let express = require('express')
+const express = require('express')
+const app = express()
 
-let app = express()
+app.use('/public',express.static('public'))
+
+app.get('/', (req, res) => res.send('<html><body><h1>This is bigger</h1></body></html>'))
+
+let jsonObj = { "name": "supreme",
+                "size": 12,
+                "crust": "deep",
+                "imgURL":  "http://www.sugardale.com/sites/default/files/stuffed%20crust%20pizza.jpg"
+            }
+                
+
+app.get('/deals', (req, res) => res.send(JSON.stringify(jsonObj)))
+
+app.listen(3000, 
+    () => console.log("Listening on port 3000"))
 
